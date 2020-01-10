95c3a74967fb92f264964c7f288856c3b060f79d
João Sousa <1160795@isep.ipp.pt>
2020-01-10 23:43:43 +0000

    Added a method to BaseController

--- FileSystem/src/controller/BaseController.java
+++ FileSystem/src/controller/BaseController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import model.FileSystem;
+
 public class BaseController {
 
     /**
@@ -10,6 +12,16 @@ public class BaseController {
      */
     public void validateName(String name) throws Exception {
         if (name.isEmpty()) throw new Exception("The name can not be empty!");
+    }
+
+    /**
+     * Method used to check if the object is read only
+     * @param object object to be checked
+     * @return true if the object is read only, false if it isn't
+     */
+    public boolean checkIfReadOnly(Object object) {
+        FileSystem fileSystem = (FileSystem) object;
+        return fileSystem.isReadOnly();
     }
 
 }
